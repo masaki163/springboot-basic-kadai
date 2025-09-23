d2aab388a669b517d180e68c101142554e9c2223
masaki163 <masaki163d@gmail.com>
2025-09-24 03:28:36 +0900

    Add files via upload

---
+++ spring_kadai_todo/src/main/java/com/example/springkadaitodo/controller/ToDoController.java
@@ -0,0 +1,28 @@
+package com.example.springkadaitodo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.springkadaitodo.entity.ToDo;
+import com.example.springkadaitodo.service.ToDoService;
+
+
+
+@Controller
+public class ToDoController {
+	private final ToDoService toDoService;
+
+    public ToDoController(ToDoService toDoService) {
+        this.toDoService = toDoService;
+    }
+
+    @GetMapping("/")
+    public String first(Model model) {
+    	 List<ToDo> todos = toDoService.getAllToDos();
+    	 model.addAttribute("todos", todos);
+        return "todoView";
+    }
+}
